18677205d1bce7a41bcf4fe31b28073abd2e8497
Lucas <lmlwojcik@inf.ufpr.br>
2025-06-19 17:07:15 -0300

    [add] confusion matrix graph

--- gen_graphs.py
+++ gen_graphs.py
@@ -1,9 +1,11 @@
 import json
 import argparse
 from bokeh.plotting import figure, save
-#from bokeh.io import export_png
-from bokeh.models.ranges import DataRange
-import cv2
+from bokeh.io import export_png
+from bokeh.models import LinearColorMapper
+from bokeh.palettes import YlGn9
+import numpy as np
+import pandas as pd
 
 def main(config, log_file, results_file):
     with open(config, "r") as fd:
@@ -27,12 +29,39 @@ def main(config, log_file, results_file):
             if cfg['save_images']:
                 fig.background_fill_color = None
                 fig.border_fill_color = None
-                #export_png(fig, f{cfg['save_dir']/g['name'].png})
+                export_png(fig, filename=f"{cfg['save_dir']}/{g['fname']}.png")
 
     if results_file is not None:
         with open(results_file, "r") as fd:
             metrics = json.load(fd)
+        margs = cfg['cm']
+        cm = pd.DataFrame(metrics['test_matrix'],
+                          index=margs['fig_args']['x_range'],
+                          columns=margs['fig_args']['y_range'])
+        cm = pd.DataFrame(cm.stack(),columns=['val']).reset_index()
+        colors = list(YlGn9)[::-1]
+        mapper = LinearColorMapper(palette=colors, low=0.0, high=1.0)
 
+        fig = figure(**margs['fig_args'])
+        fig.rect(
+            source=cm,
+            x='level_1',
+            y='level_0',
+            width=1,
+            height=1,
+            fill_color={'field':'val','transform': mapper}
+        )
+        fig.text(
+            source=cm,
+            x='level_1',
+            y='level_0',
+            text='val',
+            text_font_size='10pt',
+            x_offset=-30,
+            y_offset=10
+        )
+    
+        save(fig, "ex.html", title=margs['fig_args']['title'])
 
 
 if __name__ == '__main__':
