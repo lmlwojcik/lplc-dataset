a456183ec31f628c073240de7e8d67824d9c6fa5
t-taketsune <53000696+t-taketsune@users.noreply.github.com>
2025-06-22 18:02:14 -0300

    [add] new split scenarios + [chg] major refactor

--- gen_splits.py
+++ gen_splits.py
@@ -5,6 +5,7 @@ from pathlib import Path
 import shutil
 import argparse
 import os
+import copy
 
 def save_prot_ims(pname, pt, n_classes, exclude_idx):
     path = Path('lpr_dts') / Path(f"{pname}")
@@ -27,7 +28,7 @@ def save_ims(tr, vl, te, n_classes=4, exclude_idx=[]):
     save_prot_ims("test", te, n_classes=n_classes, exclude_idx=exclude_idx)
 
 def save_folds(tr, vl, te, idx=0, dataset_dir="lpr_dts", fold_dir="folds"):
-    path = Path(dataset_dir) / Path(f"{fold_dir}")
+    path = Path(f"{fold_dir}")
     path.mkdir(parents=True, exist_ok=True)    
 
     out = {
@@ -35,7 +36,7 @@ def save_folds(tr, vl, te, idx=0, dataset_dir="lpr_dts", fold_dir="folds"):
         "valid": vl,
         "test": te
     }
-    with open(f"{dataset_dir}/{fold_dir}/fold_{idx}.json", "w") as fd:
+    with open(f"{fold_dir}/fold_{idx}.json", "w") as fd:
         json.dump(out, fd, indent=2)
 
 
@@ -49,76 +50,146 @@ def split_files(files, n):
         i += 1
     return ret
 
-def agg_at_idxs(pls, idxs, cls=4):
+def agg_at_idxs(pls, idxs, cls):
     ret = {}
-    for i in range(cls):
+    for i in cls:
         ret[i] = []
         for idx in idxs:
-            ret[i] += pls[i][idx]
+            ret[i] += pls[int(i)][idx]
     return ret
 
-def gen_sym_partition(fs, sldir, fname, n_classes=4, exclude_idx = []):
+def gen_sym_partition(fs, sldir, fname):
     path = Path(sldir) / Path(fname)
     path.mkdir(parents=True, exist_ok=True)    
 
-    for i in range(n_classes):
-        if i in exclude_idx:
-            continue
-
+    for i in fs.keys():
         c_path = path / Path(f"{i}")
         c_path.mkdir(parents=True, exist_ok=True)
         
         for im in fs[i]:
             src = Path(im).resolve()
-            os.symlink(f"{src}", f"./{c_path}/{im.split('/')[-1]}")
+            os.symlink(f"{src}", Path(c_path) / Path(os.path.basename(im)))
 
-def gen_sym_links(tr, vl, te, fold_name, sldir, exclude_idx=[]):
-    gen_sym_partition(tr, sldir, fold_name + "/train/", n_classes=n_classes, exclude_idx=exclude_idx)
-    gen_sym_partition(vl, sldir, fold_name + "/valid/", n_classes=n_classes, exclude_idx=exclude_idx)
-    gen_sym_partition(te, sldir, fold_name + "/test/", n_classes=n_classes, exclude_idx=exclude_idx)
+def gen_sym_links(tr, vl, te, fold_name, sldir):
+    gen_sym_partition(tr, sldir, fold_name + "/train/")
+    gen_sym_partition(vl, sldir, fold_name + "/valid/")
+    gen_sym_partition(te, sldir, fold_name + "/test/")
+
+def class_mapping(c_cfg, n_classes):
+    if c_cfg is None:
+        return {i:i for i in range(n_classes)}
+    cls = {}
+    for k, v in c_cfg['class_dct'].items():
+        for c in v:
+            cls[int(c)] = int(k)
+    return cls
+
+def load_fnames(cfg, cls):
+    plates = {int(i): [] for i in cls.keys()}
+    for i in range(cfg['n_classes']):
+        if i not in cls.keys():
+            continue
+        plates[cls[i]] += sorted(list(glob(f"{cfg['dataset_dir']}/{i}/*")))
+        if cfg['do_shuffle']:
+            random.shuffle(plates[cls[i]])
+    return plates
+
+def gen_splits(cfg, c_cfg):
+    nf = cfg['n_folds']
+    if c_cfg is not None:
+        classes = c_cfg['class_dct'].keys()
+    else:
+        classes = [x for x in range(len(cfg['n_classes']))]
+    cls = class_mapping(c_cfg, cfg['n_classes'])
+    plates = load_fnames(cfg, cls)
+    for i in cls.keys():
+        plates[i] = split_files(plates[i], nf)
+
+    n_folds = {}
+    for i in range(nf):
+        valid_idx = i
+        valid_fs = agg_at_idxs(plates, [valid_idx], cls=classes)
+
+        test_idxs = []
+        for j in range(nf//2):
+            test_idxs.append((valid_idx + j + 1) % nf)
+        test_fs = agg_at_idxs(plates, test_idxs, cls=classes)
+
+        train_idxs = []
+        for j in range(nf//2):
+            train_idxs.append((valid_idx - j - 1) % nf)
+        train_fs = agg_at_idxs(plates, train_idxs, cls=classes)
+
+        save_folds(train_fs, valid_fs, test_fs, f"{i}_1",
+                   fold_dir=cfg['output_dir'])
+        n_folds[f"{i}_1"] = {"train": copy.deepcopy(train_fs),
+                             "valid": copy.deepcopy(valid_fs),
+                             "test": copy.deepcopy(test_fs)}
+
+        if cfg['cross_fold']:
+            save_folds(test_fs, valid_fs, train_fs, f"{i}_2",
+                    fold_dir=cfg['output_dir'])
+            n_folds[f"{i}_2"] = {"train": copy.deepcopy(test_fs),
+                                 "valid": copy.deepcopy(valid_fs),
+                                 "test": copy.deepcopy(train_fs)}
+
+    return n_folds
+
+def load_splits(cfg):
+    all_folds = sorted(glob(f"{cfg['output_dir']}/*.json"))
+    ret = {}
+    for f in all_folds:
+        k = f.split("_")
+        k = k[-2] + "_" + k[-1].split(".")[0]
+        with open(f, "r") as fd:
+            ret[k] = json.load(fd)
+    return ret
+
+def gen_sldirs(folds, cfg, subdir=""):
+    for k,v in folds.items():
+        gen_sym_links(v['train'], v['valid'], v['test'], k,
+                      sldir=os.path.join(cfg['sym_link_dir'], subdir))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+    parser.add_argument('--config', default=None)
+    parser.add_argument('--class_config', default=None)
+
+    parser.add_argument('--dataset_dir', default="LPSD_Dataset/lps")
+    parser.add_argument('--output_dir', default="LPSD_Dataset/folds")
+    parser.add_argument('--do_shuffle', default=False, action='store_true')
     parser.add_argument('--folds', default=5)
-    parser.add_argument('--dataset_dir', default="lpr_dts")
+    parser.add_argument('--cross_fold', default=False, action='store_true')
     parser.add_argument('--gen_sym_links', default=False, action='store_true')
-    parser.add_argument('--sym_link_dir', default="lpr_dataset")
-    parser.add_argument('--output_dir', default="folds")
-    parser.add_argument('--annotation_file', default="annotations.json")
+    parser.add_argument('--sym_link_dir', default="sldir")
+
+    parser.add_argument('--load_folds', action='store_true')
 
     args = vars(parser.parse_args())
-    with open(f"{args['annotation_file']}", "r") as fd:
-        js = json.load(fd)
+    cfg = {}
 
-    n_folds = int(args['folds'])
-    n_classes = 4
+    if args['config'] is None:
+        cfg = copy.deepcopy(args)
+    else:
+        with open(args['config'], "r") as fd:
+            cfg = json.load(fd)
+        cfg['load_folds'] = args['load_folds']
+    cfg['n_classes'] = 4
 
-    plates = {}
-    for i in range(n_classes):
-        plates[i] = sorted(list(glob(f"{args['dataset_dir']}/{i}/*")))
-        plates[i] = split_files(plates[i], n_folds)
+    if args['class_config'] is not None:
+        with open(f"{args['class_config']}", "r") as fd:
+            c_cfg = json.load(fd)
+    else:
+        c_cfg = None
 
-    for i in range(n_folds):
-        valid_idx = i
-        valid_fs = agg_at_idxs(plates, [valid_idx])
+    if not cfg['load_folds']:
+        folds = gen_splits(cfg, c_cfg)
+    else:
+        folds = load_splits(cfg)
 
-        test_idxs = []
-        for j in range(n_folds//2):
-            test_idxs.append((valid_idx + j + 1) % n_folds)
-        test_fs = agg_at_idxs(plates, test_idxs)
+    if cfg['gen_sym_links']:
+        if c_cfg is not None:
+            gen_sldirs(folds, cfg, c_cfg['sub_dir'])
+        else:
+            gen_sldirs(folds, cfg)
 
-        train_idxs = []
-        for j in range(n_folds//2):
-            train_idxs.append((valid_idx - j - 1) % n_folds)
-        train_fs = agg_at_idxs(plates, train_idxs)
-
-        save_folds(train_fs, valid_fs, test_fs, f"{i}_1", dataset_dir=args['dataset_dir'], fold_dir=args['output_dir'])
-        save_folds(test_fs, valid_fs, train_fs, f"{i}_2", dataset_dir=args['dataset_dir'], fold_dir=args['output_dir'])
-
-        if args['gen_sym_links']:
-            gen_sym_links(train_fs, valid_fs, test_fs, f"{i}_1", sldir=args['sym_link_dir'])
-            gen_sym_links(test_fs, valid_fs, train_fs, f"{i}_2", sldir=args['sym_link_dir'])
-
-        #save_ims(train_fs, valid_fs, test_fs, exclude_idx=[3])
-
-#    save_folds(test_fs, valid_fs, train_fs, i+5)
