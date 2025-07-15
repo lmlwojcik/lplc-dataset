c6cacb73434e44e9047ef4f37e4719d6f268f32b
t-taketsune <lasse1999st@gmail.com>
2025-07-15 15:52:01 -0300

    [add] correct resizing and padding

--- dataset/dataset_utils.py
+++ dataset/dataset_utils.py
@@ -1,33 +1,68 @@
 from torch.utils.data import Dataset
 from torchvision.transforms import Resize, ToTensor, Compose
 import torch
+import random
 
+import os
 import cv2
 from glob import glob
 
+
+def resize_with_pad(image, 
+                    new_shape, 
+                    padding_color = (127, 127, 127)):
+    h, w = image.shape[:2]
+    nh,nw = new_shape[:2]
+    
+    sw = nw / w
+    sh = nh / h
+    scale = min(sw, sh)
+
+    if w > nw or h > nh:
+        new_w = int(w * scale)
+        new_h = int(h * scale)
+
+        image = cv2.resize(image, (new_w, new_h), interpolation=cv2.INTER_AREA)
+    else:
+        new_w = w
+        new_h = h
+
+    delta_w = nw - new_w
+    delta_h = nh - new_h
+    top, bottom = delta_h//2, delta_h-(delta_h//2)
+    left, right = delta_w//2, delta_w-(delta_w//2)
+
+    image = cv2.copyMakeBorder(image, top, bottom, left, right, cv2.BORDER_CONSTANT, value=padding_color)
+    return image
+
 class LPSD_Dataset(Dataset):
     def __init__(self, sldir, partition, imgsz=32, device='cpu'):
-        self.files = sorted(list(glob(f"{sldir}/{partition}/*/*.jpg")))
-        self.imgsz = imgsz
-        self.transform = Compose([
-            ToTensor(),
-            Resize(imgsz)
-        ])
         if device == -1:
             device = "cpu"
         self.device = torch.device(device)
+        self.files = sorted(list(glob(f"{sldir}/{partition}/*/*.jpg")))
+        self.imgsz = imgsz
+        self.transform = Compose([
+            ToTensor()
+        ])
+        self.ims = []
+        self.lbt = []
+        self.nfs = 0
+        for f in self.files:
+            lb = int(f.split("/")[-2])
+            if not os.path.exists(f):
+                continue
+            self.nfs += 1
+            im = resize_with_pad(cv2.imread(f), imgsz)
+            self.ims.append(self.transform(im).to(self.device))
+            self.lbt.append(torch.tensor([lb]).to(self.device))
 
     def __len__(self):
-        return len(self.files)
+        return self.nfs
 
     def __getitem__(self, idx):
-        im = cv2.imread(self.files[idx])
-        im = self.transform(im)
-        im = im.to(self.device)
-
-        lb = int(self.files[idx].split("/")[-2])
-        lb = torch.tensor([lb]).to(self.device)
-
+        im = self.ims[idx]
+        lb = self.lbt[idx]
         return im, lb
 
 
