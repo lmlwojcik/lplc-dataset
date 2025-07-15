dde3438fe4f452b3966c96a4a3658e96067ef0ce
t-taketsune <lasse1999st@gmail.com>
2025-07-15 15:52:49 -0300

    [add] prediction gathering

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import argparse
+from glob import glob
 import json
 from pathlib import Path
 
-from models.models import create_yolo, create_resnet, create_vit
-from models.trainer import train_torch_model, train_yolo, test_torch_model, test_yolo
+from models.models import create_yolo, create_resnet, create_vit, create_baseline, create_base_small
+from models.trainer import train_torch_model, train_yolo, test_torch_model, test_yolo, predict_torch_model
 
-def main(config, do_train, do_test, load_model, dataset, run_name):
+def main(config, do_train, do_test, do_predict, partition, load_model, dataset, run_name):
     with open(config, "r") as fd:
         cfg = json.load(fd)
     if run_name is not None:
@@ -17,19 +18,25 @@ def main(config, do_train, do_test, load_model, dataset, run_name):
         print("Starting run")
     tag = "path" if cfg['model_name'] != 'yolo' else "dir"
     if dataset is not None:
-        cfg['data']['dir'] = dataset
+        cfg['data'][tag] = dataset
     print(f"Dataset partition: {cfg['data'][tag]}")
+    n_classes = len(glob(f"{dataset}/train/*"))
+    print(n_classes)
 
     model_name = cfg['model_name']
 
     if model_name == 'yolo':
         model = create_yolo(cfg)
     elif model_name == 'resnet':
-        model = create_resnet(cfg)
+        model = create_resnet(cfg, n_classes)
     elif model_name == 'vit':
-        model = create_vit(cfg)
+        model = create_vit(cfg, n_classes)
+    elif model_name == 'base':
+        model = create_baseline(cfg, n_classes)
+    elif model_name == 'small':
+        model = create_base_small(cfg, n_classes)
     else:
-        print("Error -- model must be one of: yolo, resnet, vit")
+        print("Error -- model must be one of: yolo, resnet, vit, base, small")
         exit()
 
     if do_train:
@@ -40,18 +47,28 @@ def main(config, do_train, do_test, load_model, dataset, run_name):
         if results is not None:
             with open(Path(cfg['train_config']['save_path']) / "train_results.json", "w") as fd:
                 json.dump(results, fd, indent=2)
+        else:
+            results = {}
     else:
         model = None
         results = {}
 
     if do_test:
         if model_name == 'yolo':
-            test_results = test_yolo(model, cfg['train_config'], cfg['data'], "test", load_model)
+            test_results = test_yolo(model, cfg['train_config'], cfg['data'], partition, load_model)
         else:
-            test_results = test_torch_model(model, cfg['test_config'], cfg['data'], "test", load_model)
+            test_results = test_torch_model(model, cfg['test_config'], cfg['data'], partition, load_model)
         results.update(test_results)
+        print(json.dumps(results, indent=2))
         with open(Path(cfg['test_config']['save_path']) / "all_results.json", "w") as fd:
             json.dump(results, fd, indent=2)
+
+    if do_predict:
+        predict_results = predict_torch_model(model, cfg['test_config'], cfg['data'], partition, load_model)
+        print(predict_results['metrics'])
+        with open(Path(cfg['test_config']['save_path']) / f"predict_results_{partition}.json", "w") as fd:
+            json.dump(predict_results, fd, indent=2)
+
 
 
 
@@ -60,6 +77,8 @@ if __name__ == '__main__':
     parser.add_argument('-c', '--config', default="configs/config_yolo.json")
     parser.add_argument('-t', '--do_train', default=False, action='store_true')
     parser.add_argument('-v', '--do_test', default=False, action='store_true')
+    parser.add_argument('-p', '--do_predict', default=False, action='store_true')
+    parser.add_argument('-pt', '--partition', default="test")
     parser.add_argument('-n', '--run_name', default=None)
     parser.add_argument('-d', '--dataset', default=None)
     parser.add_argument('-m', '--load_model', default=None)
--- models/trainer.py
+++ models/trainer.py
@@ -1,16 +1,19 @@
 import torch
 import torch.nn as nn
 from torch.optim import Adam, SGD
+from torch.optim.lr_scheduler import ReduceLROnPlateau
 from torch.utils.data import DataLoader
 from ultralytics import YOLO
 
 from pathlib import Path
 import shutil
 import json
+import os
 
 from dataset.dataset_utils import LPSD_Dataset
 from models.eval import calc_metrics, gen_metrics
 from models.utils import find_model, log_metrics_json
+
 
 def train_torch_model(model, cfg, dataset, log_cfg=None):
     save_path = Path(cfg['save_path'])
@@ -24,7 +27,7 @@ def train_torch_model(model, cfg, dataset, log_cfg=None):
 
     if cfg['validate']:
         valid_data = DataLoader(
-            LPSD_Dataset(dataset['path'], "valid", imgsz=dataset['imgsz'], device=cfg['use_gpu']),
+            LPSD_Dataset(dataset['path'], "val", imgsz=dataset['imgsz'], device=cfg['use_gpu']),
             batch_size=cfg['batch_size'],
             shuffle=cfg['shuffle']
         )
@@ -43,7 +46,6 @@ def train_torch_model(model, cfg, dataset, log_cfg=None):
     elif cfg['optim'] == "sgd":
         opt = SGD(model.named_parameters(), **cfg['optim_config'])
     loss = nn.CrossEntropyLoss()
-
     def train_epoch(epoch=0, step_update=-1, c_step=0):
         e_loss = 0
         
@@ -80,11 +82,13 @@ def train_torch_model(model, cfg, dataset, log_cfg=None):
         log_metrics['epoch'] = epoch
 
         tm = calc_metrics(model, train_data, "train")
+        print(tm)
         log_metrics.update(tm)
         log_metrics['train_loss'] = epoch_loss
 
         if cfg['validate']: 
             vm = calc_metrics(model, valid_data, "val", get_loss=True, loss=loss)
+            print(vm)
             log_metrics.update(vm)
 
         print(json.dumps(log_metrics))
@@ -120,10 +124,12 @@ def train_yolo(yolo, cfg, dataset, save_dir=None):
 
     if save_dir is not None:
         pjdir = Path(save_dir) / Path(cfg['name'])
-        cfg['project'] = pjdir
+        #pjdir = Path(save_dir)
+        cfg['project'] = Path(pjdir)
 
         if pjdir.exists():
             shutil.rmtree(pjdir)    
+        print("Project dir:", pjdir)
 
     yolo.train(data=dataset['dir'], **cfg)
 
@@ -150,6 +156,41 @@ def test_torch_model(model, cfg, dataset, partition='test', load_model=None):
     metrics = calc_metrics(model, test_data, pt=partition, return_matrix=True)
     return metrics
 
+def predict_torch_model(model, cfg, dataset, partition='test', load_model=None):
+    dts = LPSD_Dataset(dataset['path'], partition, imgsz=dataset['imgsz'], device=cfg['use_gpu'])
+    test_data = DataLoader(
+        dts,
+        batch_size=1,
+        shuffle=False
+    )
+
+    if model is None:
+        # We jump here without training, model must be loaded from memory
+        if load_model is not None:
+            model = torch.load(load_model)
+        else:
+            print(find_model(cfg['save_path']))
+            model = torch.load(find_model(cfg['save_path']))
+        if cfg['use_gpu'] != -1:
+            model.to(torch.device(f"cuda:{cfg['use_gpu']}"))
+
+    file_predicts = []
+    gts = []
+    pds = []
+    for f, (im,lb) in zip(dts.files, test_data):
+        logits = model(im)
+        lb = lb.squeeze(1)
+        pd = logits.max(1).indices
+
+        gts.append(lb)
+        pds.append(pd)
+
+        file_predicts.append({"fname": f, "gt": lb.item(), "pd": pd.item()})
+
+    metrics = gen_metrics(torch.tensor(gts),torch.tensor(pds),pt=partition,return_matrix=True)
+    ret = {'metrics': metrics, 'file_predicts': file_predicts}
+    return ret
+
 def test_yolo(model, cfg, dataset, partition='test', load_model=None):
     if model is None:
         if load_model is not None:
@@ -157,10 +198,13 @@ def test_yolo(model, cfg, dataset, partition='test', load_model=None):
         else:
             model = YOLO(f"{cfg['save_path']}/weights/best.pt")
 
-    dts = LPSD_Dataset(dataset['dir'], partition, imgsz=224, device=-1)
+    dts = LPSD_Dataset(dataset['dir'], partition, imgsz=cfg["imgsz"], device=-1)
     gts = []
     pds = []
-    for g in dts.files:
+    for idx,g in enumerate(dts.files):
+        if not os.path.exists(g):
+            continue
+    
         r = model(([g]))[0]
 
         pd = r.probs.top1
