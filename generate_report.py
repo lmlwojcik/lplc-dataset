b5bab19ae51a979fc4666031b3aefeb747f44d7d
t-taketsune <lasse1999st@gmail.com>
2025-09-02 10:21:13 -0300

    [add] report generator

---
+++ generate_report.py
@@ -0,0 +1,111 @@
+import numpy as np
+from sklearn.metrics import classification_report
+import matplotlib.pyplot as plt
+from matplotlib.transforms import Bbox
+from itertools import product
+
+from glob import glob
+import argparse
+import json
+
+def metrics_from_matrix(mat, cls):
+    metrics = {}
+    gt = []
+    pd = []
+    for i in range(len(cls)):
+        gt += [i]*sum(mat[i])
+        for j in range(len(cls)):
+            pd += [j]*mat[i][j]
+    report = classification_report(gt, pd, output_dict=True)
+
+    metrics['overall'] = report['weighted avg']['f1-score']
+    for i,c in enumerate(cls):
+        metrics[c] = report[str(i)]['f1-score']
+
+    return metrics
+
+def get_overalls(m, e, cls):
+    overalls = {}
+    for o in ['train', 'val', 'test']:
+        overalls[o] = []
+    overalls.update({c: [] for c in cls})
+    e_p = glob(f"saved/{m}/{e}_*")
+
+    for p in e_p:
+        rep = {}
+        with open(f"{p}/all_results.json", "r") as fd:
+            js = json.load(fd)
+            rep = metrics_from_matrix(js['test_matrix'], cls)
+            rep['train'] = js['train_micro_f1']
+            rep['val'] = js['val_micro_f1']
+            rep['test'] = rep.pop('overall')
+        for c in overalls.keys():
+            overalls[c].append(rep[c])
+
+    for c in overalls.keys():
+        overalls[c] = np.mean(overalls[c])
+    return overalls
+
+def gen_table(results, output):
+    ms = list(results.keys())
+    ex = list(results[ms[0]].keys())
+    metrics = list(results[ms[0]][ex[0]].keys())
+
+    data = np.zeros((len(ms), len(ex), len(metrics)))
+    for i,m in enumerate(ms):
+        for j,e in enumerate(ex):
+            for k,p in enumerate(metrics):
+                data[i,j,k] = results[m][e][p]
+    first_rows = list(product(ms, ex))
+    print(len(first_rows))
+
+    # Arguments for table
+    voff = 1
+    hsiz = 0.27*len(metrics)        # total table width
+    #hsiz = 0.27*(len(metrics)+2)
+    clv = 0.085                  # cell height
+    #voff -= (len(first_rows)+1)*clv # controls vertical delta
+    print(data)
+    plt.axis('off')
+    plt.grid('off')
+
+    h1 = plt.table(
+        cellText=[list(first_rows[i]) + [f"{x:.4g}" for x in data[i//len(ms),i-(i//len(ms)*len(ms))]]
+                   for i in range(len(first_rows))],
+        colLabels=["Model", "Scenario"] + metrics,
+        colWidths= [(0.35*len(x))/(len(ms[0]) + len(ex[0])) for x in [ms[0], ex[0]]] + [0.65/len(metrics)]*len(metrics),
+        bbox=[0, voff, hsiz, clv*(len(first_rows)+1)]
+    )
+
+    h1.auto_set_font_size(False)
+    h1.set_fontsize(12)
+    voff += clv
+
+    h1.figure.savefig(f"saved/figs/{output}.png", transparent=False, bbox_inches='tight')
+
+def main(models, experiments, class_config, output):
+    with open(f"configs/split_configs/{class_config}", "r") as fd:
+        cls = json.load(fd)['class_names']
+
+    rs = {}
+    for m in models:
+        rs[m] = {}
+        for i,e in enumerate(experiments):
+            results = get_overalls(m, e, cls)
+            rs[m][e] = results
+
+    gen_table(rs, output)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-m", "--models", nargs='+', default=[], required=True)
+    parser.add_argument('-x', '--experiments', nargs='+', default=[])
+
+    parser.add_argument("-c", "--class_config", default=['base.json'])
+
+    parser.add_argument("-o", "--output", default='report')
+    
+    args = vars(parser.parse_args())
+
+    main(**args)
